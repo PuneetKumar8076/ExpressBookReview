18a1bdcca871ac7afc4da13c8d1f4f4c7dfc5dce
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-20 01:49:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+// Routes
 const general = require('./routes/general');
+const auth_users = require('./routes/auth_users');
 
+// Middleware
 app.use(express.json());
 
+// Routes use
 app.use("/", general);
+app.use("/customer", auth_users);
 
+// Server
 app.listen(5000, () => {
- console.log("Server running on port 5000");
+  console.log("Server running on port 5000");
 });
