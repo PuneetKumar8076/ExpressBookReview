7b65c92828314dcf478c815b93164b406f8a4fc5
PUNEET KUMAR <kumarmahesh3594@gmail.com>
2026-03-20 02:02:56 +0530

    Update auth_users.js

--- routes/auth_users.js
+++ routes/auth_users.js
@@ -1,96 +1,48 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-let books = require('../booksdb');
+const axios = require('axios');
 
-const regd_users = express.Router();
-regd_users.use(express.json());
-
-let users = [];
-
-// check username exists
-const isValid = (username) => {
-  return users.some(user => user.username === username);
-};
-
-// check login
-const authenticatedUser = (username, password) => {
-  return users.some(user => user.username === username && user.password === password);
-};
-
-// Q6: Register
-regd_users.post("/register", (req, res) => {
-  const { username, password } = req.body;
-
-  if (username && password) {
-    if (!isValid(username)) {
-      users.push({ username, password });
-      return res.json({ message: "User successfully registered" });
-    } else {
-      return res.status(404).json({ message: "User already exists" });
+// Get all books
+async function getAllBooks() {
+    try {
+        const response = await axios.get('http://localhost:5000/books');
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-  }
+}
 
-  return res.status(404).json({ message: "Unable to register user" });
-});
-
-// Q7: Login
-regd_users.post("/login", (req, res) => {
-  const { username, password } = req.body;
-
-  if (authenticatedUser(username, password)) {
-    let token = jwt.sign({ username }, "access", { expiresIn: 60 * 60 });
-    return res.json({ message: "User successfully logged in", token });
-  } else {
-    return res.status(401).json({ message: "Invalid login" });
-  }
-});
-
-// middleware
-const verifyToken = (req, res, next) => {
-  const authHeader = req.headers['authorization'];
-
-  console.log("AUTH HEADER:", authHeader);  //  check 1
-
-  if (!authHeader) return res.sendStatus(401);
-
-  const token = authHeader.split(' ')[1];
-
-  console.log("TOKEN:", token);             //  check 2
-
-  jwt.verify(token, "access", (err, user) => {
-    if (err) {
-      console.log("JWT ERROR:", err);       //  check 3
-      return res.sendStatus(403);
+// Get book by ISBN
+async function getBookByISBN(isbn) {
+    try {
+        const response = await axios.get(`http://localhost:5000/isbn/${isbn}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
+}
 
-    req.user = user;
-    next();
-  });
+// Get books by Author
+async function getBookByAuthor(author) {
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${author}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Get books by Title
+async function getBookByTitle(title) {
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${title}`);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    getAllBooks,
+    getBookByISBN,
+    getBookByAuthor,
+    getBookByTitle
 };
-
-// Q9: Add review
-regd_users.put("/auth/review/:isbn", verifyToken, (req, res) => {
-  const isbn = req.params.isbn;
-  const review = req.body.review;
-  const username = req.user.username;
-
-  if (!books[isbn].reviews) {
-    books[isbn].reviews = {};
-  }
-
-  books[isbn].reviews[username] = review;
-
-  return res.json({ message: "Review added/updated successfully" });
-});
-
-// Q10: Delete review
-regd_users.delete("/auth/review/:isbn", verifyToken, (req, res) => {
-  const isbn = req.params.isbn;
-  const username = req.user.username;
-
-  delete books[isbn].reviews[username];
-
-  return res.json({ message: "Review deleted successfully" });
-});
-
-module.exports = regd_users;
